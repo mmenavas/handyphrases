31a7eb52510c86bd38a1da81ee1aa4144a2d24fa
Maximo Mena <maximo.mena@meltmedia.com>
2016-04-24 18:48:48 -0700

    Attempt to create a block with an embedded form to create Phrase nodes.

---
+++ modules/custom/handyphrases_blocks/src/Plugin/Block/AddPhraseBlock.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Drupal\handyphrases_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\NodeType;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\EntityFormBuilderInterface;
+
+
+/**
+ * Provides a block with a form to add a new Phrase.
+ *
+ * @Block(
+ *  id = "add_phrase_block",
+ *  admin_label = @Translation("Add Phrase Block"),
+ *  provider = "node"
+ * )
+ */
+class AddPhraseBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity manager.
+   *
+   * @var \Drupal\Core\Entity\EntityManagerInterface
+   */
+  protected $entityManager;
+
+  /**
+   * The entity form builder.
+   *
+   * @var \Drupal\Core\Entity\EntityFormBuilderInterface.
+   */
+  protected $entityFormBuilder;
+
+  /**
+   * Constructs a new NodeFormBlock plugin
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityManagerInterface $entityManger
+   *   The entity manager.
+   * @param \Drupal\Core\Entity\EntityFormBuilderInterface $entityFormBuilder
+   *   The entity form builder.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityManagerInterface $entityManager, EntityFormBuilderInterface $entityFormBuilder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->setConfiguration($configuration);
+    $this->entityManager = $entityManager;
+    $this->entityFormBuilder = $entityFormBuilder;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity.manager'),
+      $container->get('entity.form_builder')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+    $form = array();
+
+    $entity_type = 'node';
+    $bundle = 'phrase';
+    $form_mode = 'node.add_phrase';
+
+    $node_type = NodeType::load($bundle);
+
+    $node = $this->entityManager->getStorage($entity_type)->create(array(
+      'type' => $node_type->id(),
+    ));
+
+
+
+    $form = $this->entityFormBuilder->getForm($node, 'default', array(
+      'mode' => $form_mode
+    ));
+
+//    $phrase = \Drupal::entityTypeManager()
+//      ->getStorage('entity_form_display')
+//      ->create(array(
+//        'entity_form_display' => array(
+//          'targetEntityType' => $entity_type,
+//          'bundle' => $bundle,
+//          'mode' => $form_mode,
+//          'status' => TRUE,
+//        )
+//      ));
+//
+
+//    $phrase = entity_get_form_display($entity_type, $bundle, $form_mode);
+//
+//    $form = $phrase->buildForm();
+////    $form = \Drupal::service('entity.form_builder')->getForm($phrase);
+
+    return $form;
+  }
+}
